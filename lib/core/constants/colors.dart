a75ab078f94dcaa4d78a98aedec0311ad2f07bd6
sifou20ws <blvsifo3@gmail.com>
2024-03-14 21:49:38 +0100

    add constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+// Black
+Color get black900 => Color(0XFF000000);
+
+// BlueGray
+Color get blueGray100 => Color(0XFFD9D9D9);
+Color get blueGray200 => Color(0XFFB5BFD6);
+Color get blueGray700 => Color(0XFF4A3F69);
+Color get blueGray70001 => Color(0XFF52525B);
+
+// DeepPurple
+Color get deepPurpleA200 => Color(0XFF9747FF);
+
+// Gray
+Color get gray200 => Color(0XFFEAE9E9);
+Color get gray20001 => Color(0XFFEBEAEA);
+Color get gray50 => Color(0XFFFAFAFA);
+
+// Pink
+Color get pink500 => Color(0XFFDB2777);
+
+// White
+Color get whiteA700 => Color(0XFFFFFFFF);
+
+const primaryText = Color(0xFF27272A);
+const pink100 = Color(0xFFFCE7F3);
+const pink600 = Color(0xFFDB2777);
+const pink700 = Color(0xFFBE185D);
+const secondaryPink = Color(0xFFDB2777);
+
+const rose100 = Color(0xFFFFE4E6);
+
+
+const primaryWhite = Color(0xFFFFFFFF);
+
+const zinc400 = Color(0xFFA1A1AA);
+const zinc300 = Color(0xFFD4D4D8);
+const zinc500 = Color(0xFF71717A);
+const zinc600 = Color(0xFF52525B);
+const zinc700 = Color(0xFF3F3F46);
+const zinc900 = Color(0xFF18181B);
+
+// Gray
+const grey40 = Color(0xFF404040);
+
+Color get gray300 => Color(0XFFE4E4E7);
+Color get gray500 => Color(0XFFA1A1AA);
+Color get gray600 => Color(0XFF71717A);
+Color get gray800 => Color(0XFF3F3F46);
+Color get gray900 => Color(0XFF27272A);
+Color get gray8000f => Color(0X0F404040);
+
+// Teal
+Color get teal100 => Color(0XFFCCFBF1);
+Color get teal500 => Color(0XFF14B8A6);
+Color get teal600 => Color(0XFF0D9488);
+
+// BlueGray
+Color get blueGray10001 => Color(0XFFD4D4D8);
+Color get lightBlue => Color(0XFF0284C7);
+
+// DeepOrange
+Color get deepOrange50 => Color(0XFFFFE4E6);
+
+Color get purple600 => Color(0XFF9333EA);
+
+Color get yellow500 => Color(0XFFEAB308);
+
---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,20 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Accueil images
+  static String logo = '$imagePath/logo.png';
+
+  // Common images
+  static String imgArrowLeft = '$imagePath/arrow_left.svg';
+
+  static String imgSearch = '$imagePath/img_search.svg';
+
+  static String imgSave = '$imagePath/send.svg';
+
+  static String imgUserActive = '$imagePath/user_active.svg';
+
+  static String imgUser = '$imagePath/user.svg';
+
+  static String imgNotFound = 'assets/images/image_not_found.png';
+}
