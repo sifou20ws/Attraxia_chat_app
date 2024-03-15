f592f958a3206188d15585cf8c13d1d189ef3051
sifou20ws <blvsifo3@gmail.com>
2024-03-15 19:11:45 +0100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:attraxia_chat_app/routes/app_routes.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:attraxia_chat_app/core/app_export.dart';
+
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
